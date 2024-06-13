eb73cf39aeef0c310f262e905c1e8967bffa8d46
JacobG321 <jacobgruber1998@gmail.com>
2024-06-12 21:49:41 -0400

    initial commit

---
+++ github-public-repo-updater.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: GitHub Public Repo Updater
+ * Plugin URI: https://github.com/JacobG321/wp-plugin-update-from-repo
+ * Description: A simple plugin to update a public GitHub repository.
+ * Version: 0.0.1
+ * Author: Jacob Gruber
+ * Author URI: jgruber.dev
+ */
+
+
